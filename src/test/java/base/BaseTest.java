5da663457148766eb533bc9366e5d60698171dfe
Thai Luu <thai.luu@agest.vn>
2024-12-13 15:25:28 +0700

    Support config browser and parallel

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,21 +1,32 @@
 package base;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utils.BrowserFactory;
 
 public class BaseTest {
 
+    private WebDriver driver;
+
     @Parameters("browser")
     @BeforeMethod
-    public void setUp(String browser) {
-        // Lấy browser từ tham số testng.xml
-        Configuration.browser = browser;
-        Configuration.browserSize = ("true");
+    public void setUp(@Optional("chrome") String browser) {
+        String browserFromMaven = System.getProperty("browser");
+        if (browserFromMaven != null && !browserFromMaven.isEmpty()) {
+            browser = browserFromMaven;
+        }
+        driver = BrowserFactory.createDriver(browser);
+        WebDriverRunner.setWebDriver(driver);
     }
 
     @AfterMethod
     public void tearDown() {
-        WebDriverRunner.closeWebDriver();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
